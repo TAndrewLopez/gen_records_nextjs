4455d90da6796d9bbde88773c15a6904c5d8fb98
Andrew Dobson <tandrewlopez@gmail.com>
2023-01-10 22:50:50 -0500

    attempting to fix pg issue on vercel

--- server/conn.js
+++ server/conn.js
@@ -1,8 +1,9 @@
 const Sequelize = require("sequelize");
+const pg = require("pg");
 
 const conn = new Sequelize(
   process.env.DATABASE_URL || "postgres://localhost:5432/recordShop",
-  { logging: false }
+  { dialect: "postgres", dialectModule: pg, logging: false }
 );
 
 module.exports = conn;
