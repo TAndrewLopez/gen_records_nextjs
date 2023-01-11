b24889d7c5c38a7a988870e8b841624739af0be7
Andrew Dobson <tandrewlopez@gmail.com>
2023-01-11 14:00:40 -0500

    added checkout route for signed in users

---
+++ pages/api/shop/checkout.js
@@ -0,0 +1,38 @@
+import { requireToken } from "../../../customMiddleware";
+import { LineItem, Order, User, Vinyl } from "../../../server";
+
+const handler = async (req, res) => {
+  if (req.method === "PUT") {
+    try {
+      const order = await Order.findOne({
+        where: {
+          userId: req.user.id,
+          complete: false,
+        },
+        include: LineItem,
+      });
+
+      if (!order.lineItems.length) {
+        return res
+          .status(401)
+          .json({ success: false, message: "Add items to cart for checkout." });
+      }
+
+      order.lineItems.forEach(async (item) => {
+        const product = await Vinyl.findByPk(item.vinylId);
+        await product.update({ stock: product.stock - item.qty });
+      });
+
+      await order.update({ complete: true });
+      await Order.create({ userId: req.user.id });
+      res.status(200).json({
+        success: true,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ success: false, message: error.message });
+    }
+  }
+};
+
+export default requireToken(handler);
