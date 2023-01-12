1ff514c8d06889e843056f3bc47775f0cbae3668
Andrew Dobson <tandrewlopez@gmail.com>
2023-01-11 19:59:37 -0500

    connected user profile form

---
+++ pages/api/auth/[userId].js
@@ -0,0 +1,24 @@
+import { User } from "../../../server";
+import { requireToken } from "../../../customMiddleware";
+
+const handler = async (req, res) => {
+  if (req.method === "PUT") {
+    try {
+      const user = await User.findByPk(req.user.id);
+      const { firstName, lastName, username, email, password, img } = req.body;
+      await user.update({
+        firstName: firstName || req.user.firstName,
+        lastName: lastName || req.user.lastName,
+        username: username || req.user.username,
+        email: email || req.user.email,
+        password: password || req.user.password,
+        img: img || req.user.img,
+      });
+      res.status(200).json({ success: true, user });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ success: false, message: error.message });
+    }
+  }
+};
+export default requireToken(handler);
